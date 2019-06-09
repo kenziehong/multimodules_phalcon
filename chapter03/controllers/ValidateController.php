4443e618d99d68c08ad96936440d404939af9625
Hong <dinhthianhhong.uel@gmail.com>
2019-06-09 23:46:12 +0700

    validate form-jquery

---
+++ chapter03/controllers/ValidateController.php
@@ -0,0 +1,13 @@
+<?php
+namespace Multiphalcon\Chapter03\Controllers;
+
+class ValidateController extends \Phalcon\Mvc\Controller {
+    public function indexAction(){
+        
+    }
+
+    public function index1Action(){
+        
+    }
+
+}
