204bec1cfaf8923088caef7e4bd6e204bccb2d52
Hong <dinhthianhhong.uel@gmail.com>
2019-06-21 21:47:57 +0700

    VUE-add el,data into vue_ticket.js

---
+++ resources/js/vue_ticket.js
@@ -0,0 +1,13 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      ticketQuantity: 1,
+      ticketType: 'general',
+      referrals: [],
+      specialRequests: '',
+      purchaseAgreementSigned: false,
+    },
+  });
