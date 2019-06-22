a4f47237ad44644767c97efd5910f0b70fa7196f
Hong <dinhthianhhong.uel@gmail.com>
2019-06-23 00:28:46 +0700

    vue-master: created component - product-review - add form

--- resources/js/vue_sock.js
+++ resources/js/vue_sock.js
@@ -101,11 +101,38 @@ Vue.component('product',{
 
 Vue.component('product-review', {
     template: `
-        <input v-model="name">
+        <form class="review-form">
+            <p>
+                <label for="name">Name:</label>
+                <input id="name" placeholder="name">
+            </p>
+            
+            <p>
+                <label for="review">Review:</label>      
+                <textarea id="review"></textarea>
+            </p>
+            
+            <p>
+                <label for="rating">Rating:</label>
+                <select id="rating">
+                    <option>5</option>
+                    <option>4</option>
+                    <option>3</option>
+                    <option>2</option>
+                    <option>1</option>
+                </select>
+            </p>
+                
+            <p>
+                <input type="submit" value="Submit">  
+            </p>    
+        </form>
     `,
     data() {
         return {
-            name: null
+            name: null,
+            review: null,
+            rating: null,
         }
     }
 
