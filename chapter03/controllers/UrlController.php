09994d7c30c24d15c91e8374df3e20d228e4c835
Hong <dinhthianhhong.uel@gmail.com>
2019-07-04 23:16:17 +0700

    PHALCON_SetBaseUrl_Lesson 131

--- chapter03/controllers/UrlController.php
+++ chapter03/controllers/UrlController.php
@@ -7,11 +7,21 @@ class UrlController extends \Phalcon\Mvc\Controller {
         //create url, link to an image
         $imageUrl = $this->url->get('resources/image/redrose.jpg');
         $cssUrl = $this->url->get('resources/css/me.css');
-
+        
         echo 'Image Url: '.$imageUrl;
         echo '<img src=" '.$imageUrl.' " width="300px" height="200px" >';
-
+        
         echo'<link rel="stylesheet" href=" '.$cssUrl.' ">';
+    }
+
+    public function index2Action() {
+        //$baseUrl = $this->url->getBaseUrl(); //display a slash
+        $this->url->setBaseUri('chapter04/url');
+        $baseUrl = $this->url->getBaseUri(); 
+        $imageUrl = $this->url->get('resources/image/redrose.jpg');
+        echo $baseUrl;
+        echo '<hr>';
+        echo $imageUrl;
     }
 }
 
