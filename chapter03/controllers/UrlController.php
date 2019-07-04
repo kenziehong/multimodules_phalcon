796e55391c437f6ef39e9225156f0e499cde317b
Hong <dinhthianhhong.uel@gmail.com>
2019-07-04 23:40:57 +0700

    PHALCON_Url_Path_Params_Lesson 132

--- chapter03/controllers/UrlController.php
+++ chapter03/controllers/UrlController.php
@@ -23,5 +23,30 @@ class UrlController extends \Phalcon\Mvc\Controller {
         echo '<hr>';
         echo $imageUrl;
     }
+
+    public function index3Action() {
+        $infoUrl = $this->url->get('chapter03/url/info', [
+            'book_name' => 'phalcon book',
+            'price' => 600,
+        ]);
+
+        $zendvnUrl = $this->url->get('https://www.google.com/', [], true); //cannot access outside local
+        $zendvnUrl2 = $this->url->get('https://www.google.com/', [], false); //can access outside local
+        echo '<a href=" '.$infoUrl.' ">go-to-infoAction</a>';
+        echo '<hr>';
+        echo '<a href=" '.$zendvnUrl.' ">go-to-zendvn-local</a>';
+        echo '<hr>';
+        echo '<a href=" '.$zendvnUrl2.' ">go-to-zend</a>';
+    }
+
+    public function infoAction() {
+        echo '<h3 style="color:red">' . __METHOD__ .'</h3>';
+        $book_name =  $this->request->getQuery('book_name'); //getPost, take data from FORM
+        $price = $this->request->getQuery('price');
+
+        echo 'Book Name: '. $book_name;
+        echo '<hr>';
+        echo 'Price: '. $price;
+    }
 }
 
