03f9a691046765033ec0cdfe51364db660e89bd0
kenziehong <dinhthianhhong.uel@gmail.com>
2019-07-09 23:32:05 +0700

    PHALCON_Dispatcher-forward-display-other-action-pass-params-Lesson106

--- chapter03/controllers/DispatcherController.php
+++ chapter03/controllers/DispatcherController.php
@@ -19,9 +19,35 @@ class DispatcherController extends \Phalcon\Mvc\Controller{
         ]);
     }
 
+    public function index2Action(){
+        echo '<br>=============index2Action==============Start</br>';
+        echo '<h3 style="color:red">'.__METHOD__.'</h3>';
+        echo '<br>=============inde2xAction==============End</br>';
+        //show include detailAction
+        $this->dispatcher->forward([
+            'action' => 'detail',
+            'params' => [
+                'name' => 'zendvn',
+                'color' => 'green',
+            ],
+
+        ]);
+    }
+
     public function detailAction(){
         echo '<br>=============detailAction==============Start</br>';
-        echo '<h3 style="color:red">'.__METHOD__.'</h3>';
+            echo '<h3 style="color:red">'.__METHOD__.'</h3>';
+            
+            $params = $this->dispatcher->getParams();
+            $name = $this->dispatcher->getParam('name', 'upper', 'default name');
+            $color = $this->dispatcher->getParam('abc', null, 'default color');
+            echo '<pre>';
+                print_r($params);
+            echo '</pre>';
+            
+            echo '<h3 style="color:red">'.$name.'</h3>';
+            echo '<h3 style="color:red">'.$color.'</h3>';
+
         echo '<br>=============detailAction==============End</br>';
 
     }
