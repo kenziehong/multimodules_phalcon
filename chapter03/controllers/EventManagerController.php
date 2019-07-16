5f4addf7cb521df2e34944ba58483a7c2782e2c4
kenziehong <dinhthianhhong.uel@gmail.com>
2019-07-16 22:43:21 +0700

    PHALCON_EventManager-attach many jobs-Lesson 75

--- chapter03/controllers/EventManagerController.php
+++ chapter03/controllers/EventManagerController.php
@@ -21,4 +21,30 @@ class EventManagerController extends \Phalcon\Mvc\Controller {
         $eventManager->fire('event:02', $this); //source $this
         
     }
+
+    public function index2Action() {
+        //echo __METHOD__;
+
+        $eventManager = new Manager();
+
+        $eventManager->attach('event:01', function(){
+            echo '<h3 style="color:red">event 01 - doing 01</h3>';
+        });
+
+        $eventManager->attach('event:01', function(){
+            echo '<h3 style="color:red">event 01 - doing 02</h3>';
+        });
+
+        $eventManager->attach('event:02', function(){
+            echo '<h3 style="color:red">event 02 - doing 01</h3>';
+        });
+
+        $eventManager->attach('event:02', function(){
+            echo '<h3 style="color:red">event 02 - doing 02</h3>';
+        });
+
+        $eventManager->fire('event:01', $this); //source $this
+        $eventManager->fire('event:02', $this); //source $this
+        
+    }
 }
